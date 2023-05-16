7a9543d595ea645940eb053da0cb0890740fab91
Maderamel <maderam96@gmail.com>
2023-05-16 14:51:37 -0700

    added updates from session with tutor

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,67 @@
+// Use the D3 library to read in samples.json from the URL 
+const samples = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+// function for initial landing page
+function init() {
+
+let dropDown = d3.select("#selDataset");
+
+// Fetch the JSON data and console log it
+d3.json(samples).then((data)=> {
+  let sampleData = data.names;
+
+  for(let i = 0; i < sampleData.length; i++) {
+    dropDown.append("option").text(sampleData[i]).property("value", sampleData[i]);
+  };
+
+  console.log(sampleData);
+});
+let first = sampleData[0];
+buildchart(first);
+buildmetadata(first);
+}
+
+// build metadata function
+function buildMetadata(sample) {
+
+  d3.json(samples).then((data)=> {
+    let metaData = data.metadata;
+    // look for oject with same sample number
+    let resultData = metaData.filter((sampleNum)=> sampleNum.id == sample);
+    let sampResult = resultData[0];
+    let demoPanel = d3.select("#sample-metadata");
+    demoPanel.html("");
+    Object.enteries(sampResult).forEach(([key, value]) => {demoPanel.append("h6").text(`${key}: ${value}`);
+  });
+   
+  };
+}
+
+// bar chart function
+function barChart(sample) {
+  
+  d3.json(samples).then((data)=> {
+    let sampleInfo = data.samples;
+
+    let filterSamples = sampleInfo.filter(sampObj => sampObj.id == sample);
+    let resultData = filterSamples[0];
+    let sample_values = resultData.sample_values;
+    let otu_ids = resultData.otu_ids;
+    let otu_labels = resultData.otu_labels;
+
+    let trace1 = {
+      x: resultData.slice(0,10).reverse(),
+      y: otu_ids.slice(0,10).reverse(),
+      text: otu_labels.slice(0,10).reverse(),
+      name: "Greek",
+      type: "bar",
+      orientation: "h"
+    };
+    Plotly.newPlot("bar", resultData, layout);
+    
+  });
+
+}
+
+
+init();
